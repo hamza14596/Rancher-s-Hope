cb441121f3dbc9904832663a422fbc4a94fe36b6
hamza <hmzuuu87@gmail.com>
2025-07-26 16:14:42 +0500

    just initialized the window

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame, sys
+from sys import *
+
+class Game:
+    def __init__(self): 
+        pygame.init()
+        pygame.display.set_caption("Rancher's Hope")
+        
+        self.screen = pygame.display.set_mode((1430,700))
+        self.clock = pygame.time.Clock()
+
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.QUIT()
+                    sys.EXIT()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()

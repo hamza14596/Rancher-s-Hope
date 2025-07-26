4794721e6fa818234e8c8a021c8c0f9f26d573b0
hamza <hmzuuu87@gmail.com>
2025-07-27 02:01:33 +0500

    added player movement, fixed bugs, diagonal movement was extremely fast comapred to horiontal and vertical, so i also fixed that

--- Rancher.py
+++ Rancher.py
@@ -1,14 +1,50 @@
 import pygame
 from settings import * 
+
 class  Player(pygame.sprite.Sprite):
      def  __init__(self,position,group):
           super().__init__(group)
 
-          self.image = pygame.Surface((64, 32))
+          self.image = pygame.Surface((32, 64))
           self.image.fill('green')
           self.rect = self.image.get_rect(center = position)
-    
 
-        
+          self.direction = pygame.math.Vector2()
+          self.position = pygame.math.Vector2(self.rect.center)   
+          self.speed = 200
+
+     def input(self):
+          keys = pygame.key.get_pressed()
+          if keys[pygame.K_UP]:
+              self.direction.y= -1
+          elif keys[pygame.K_DOWN]:
+              self.direction.y = 1
+          else:
+                self.direction.y = 0
+
+
+          if keys[pygame.K_LEFT]:
+              self.direction.x= -1
+          elif keys[pygame.K_RIGHT]:
+              self.direction.x = 1
+          else:
+                    self.direction.x = 0    
+          
+     def move(self,dt):
+          if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
+
+
+          self.position.x += self.direction.x * self.speed * dt
+          self.rect.centerx = self.position.x
+
+          self.position.y += self.direction.y * self.speed * dt
+          self.rect.centery = self.position.y
+          
+          
+
+     def update(self,dt):
+          self.input()
+          self.move(dt)
 
     

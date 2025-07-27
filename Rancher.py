cb52b0e4fe741782814ac26c8f54b42d2a563e70
hamza <hmzuuu87@gmail.com>
2025-07-27 14:53:18 +0500

    added the graphics, imported them in my workspace and finally got the hang of it, the sprite is now visibleand playable

--- Rancher.py
+++ Rancher.py
@@ -5,11 +5,14 @@ from help import *
 class  Player(pygame.sprite.Sprite):
      def  __init__(self,position,group):
           super().__init__(group)
+          
           self.import_assests()
+          self.status = 'down_idle'
+          self.frame_index = 0
 
 
-          self.image = pygame.Surface((32, 64))
-          self.image.fill('green')
+
+          self.image = self.animations[self.status][self.frame_index]
           self.rect = self.image.get_rect(center = position)
 
           self.direction = pygame.math.Vector2()
@@ -19,13 +22,12 @@ class  Player(pygame.sprite.Sprite):
           self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
                             'right_idle': [], 'left_idle': [], 'up_idle': [], 'down_idle': [],
                             'right_hoe': [], 'left_hoe': [], 'up_hoe': [], 'down_hoe': [],
-                            'right_axe': [], 'left_axe': [], 'up_axe': [], 'down_axe': [],
-                            'right_water': [], 'left_water': [], 'up_water': [], 'down_water': []
-          }
+                            'right_axe': [], 'left_axe': [], 'up_axe': [], 'down_axe': [],}
+             
           for animation in self.animations.keys():
-                full_path = f'graphics/character{animation}'
+                full_path = '../graphics/character/' + animation + '/'
                 self.animations[animation] = import_folder(full_path)
-
+          print(self.animations)
      def input(self):
           keys = pygame.key.get_pressed()
           if keys[pygame.K_UP]:
